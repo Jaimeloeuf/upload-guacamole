f3126562398fc46f68ba73a396c6b2cc5a8d2798
Jaime <jaimeloeuf@gmail.com>
2024-06-09 20:24:30 +0800

    Update app to read path from CLI arg.

--- src/main.go
+++ src/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+func getPath() (string, error) {
+	// First element in os.Args is always the program name,
+	// So we need at least 2 arguments to have a file name argument.
+	if len(os.Args) < 2 {
+		fmt.Println("Missing parameter, please provide path")
+		return "", errors.New("missing parameter, please provide path")
+	}
+
+	return os.Args[1], nil
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	path, err := getPath()
+
+	if err != nil {
+		return
+	}
+
+	fmt.Print(path)
 }
